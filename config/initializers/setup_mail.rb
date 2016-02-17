4d2d8e939879b0c163c959e135c62329f25ec10f
Rolph DZOUNGA <rolphfrancel@gmail.com>
2016-02-17 12:14:23 +0100

    000

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
 	:address => ENV['APP_SMTP'],
-	:port => ENV['A_PORT'],
+	:port => '587',
 	:domain => ENV['A_DOMAIN'] ,
 	:user_name => ENV['S_USERNAME'],
 	:password => ENV['SENDGRID_PASSWORD'],
