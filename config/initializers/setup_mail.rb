f072be78b66fe0e4ce88369143eea21fa714be68
Rolph DZOUNGA <rolphfrancel@gmail.com>
2016-02-17 13:07:33 +0100

    hum

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
 	:address => ENV['APP_SMTP'],
 	:port => '587',
 	:domain => ENV['A_DOMAIN'] ,
-	:user_name => ENV['S_USERNAME'],
-	:password => ENV['SENDGRID_PASSWORD'],
+	:user_name => ENV['AB_USERNAME'],
+	:password => ENV['AB_PASSWORD'],
 	:authentication => 'plain',
 	:enable_starttls_auto => true
 }
