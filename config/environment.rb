80de1f70a6408a6aad7dda6f5f50044f7ea4bd2b
Rolph DZOUNGA <rolphfrancel@gmail.com>
2016-02-18 00:00:23 +0100

    OKK

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
+ActionMailer::Base.smtp_settings = {
+	:address => ENV['APP_SMTP'],
+	:port => '587',
+	:domain => ENV['A_DOMAIN'] ,
+	:user_name => ENV['AB_USERNAME'],
+	:password => ENV['AB_PASSWORD'],
+	:authentication => 'plain',
+	:enable_starttls_auto => true
+}
--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-	:address => ENV['APP_SMTP'],
-	:port => '587',
-	:domain => ENV['A_DOMAIN'] ,
-	:user_name => ENV['AB_USERNAME'],
-	:password => ENV['AB_PASSWORD'],
-	:authentication => 'plain',
-	:enable_starttls_auto => true
-}
